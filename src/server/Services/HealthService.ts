bc128153887e1d3aa5e1cad6962ba2d998460924
AlphaRunic <roleyspersonal@gmail.com>
2022-08-27 01:05:50 -0700

    Fix health sometimes not updating

--- src/server/Services/HealthService.ts
+++ src/server/Services/HealthService.ts
@@ -12,29 +12,21 @@ const HealthService = Knit.CreateService({
     Name: "HealthService",
 
     KnitStart() {
-        let firstTime = true;
         Players.PlayerAdded.Connect(p => {
-            let conn: RBXScriptConnection;
-            conn = p.CharacterAdded.Connect(plrChar => {
-                const data = Knit.GetService("DataService");
-                const chars = Knit.GetService("CharacterService");
-                data.DataUpdated.Connect((plr: Player, name: string, equippedChar) => {
-                    if (name === "equippedCharacter" && p === plr) {
-                        task.spawn(() => {
-                            if (firstTime) {
-                                task.wait(3);
-                                firstTime = false;
-                            }
-                            const partySetup = <string[]>data.Get(plr, "partySetup");
-                            const charName = partySetup[<number>equippedChar - 1];
-                            const char = chars.GetData(charName)!;
-                            const hum = plrChar?.FindFirstChildOfClass("Humanoid")!;
-                            hum.MaxHealth = char.State.Stats.MaxHealth;
-                            hum.Health = char.State.Stats.Health;
-                        });
-                    }
-                });
-                conn.Disconnect();
+            const data = Knit.GetService("DataService");
+            data.DataUpdated.Connect((plr: Player, name: string, equippedChar) => {
+                if (name === "equippedCharacter" && p === plr) {
+                    const chars = Knit.GetService("CharacterService");
+                    task.delay(10, () => {
+                        const partySetup = <string[]>data.Get(plr, "partySetup");
+                        const charName = partySetup[<number>equippedChar - 1];
+                        const char = chars.GetData(charName)!;
+                        const plrChar = plr.Character || plr.CharacterAdded.Wait()[1];
+                        const hum = <Humanoid>plrChar?.WaitForChild("Humanoid")!;
+                        hum.MaxHealth = char.State.Stats.MaxHealth;
+                        hum.Health = char.State.Stats.Health;
+                    });
+                }
             });
         });
     },
